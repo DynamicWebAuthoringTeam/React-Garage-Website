d0ff5ef24f046e6017c184223a239917da021670
Lewis-White <78178876+Lewis-White@users.noreply.github.com>
2021-04-11 19:58:58 +0100

    contact new

--- src/component/page/Contact.js
+++ src/component/page/Contact.js
@@ -1,25 +1,44 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import { Col, Container, Form, Button } from 'react-bootstrap';
 import './page-css/Contact.css';
+
 
 function Contact() {
     return (
-        <div className="contact">
-            <p>Contact Me</p>
-            
-                <div className ="contact-form">
-                    <div className="first-name">
-                        
-                        <input type="text" id="fname" name="firstname" placeholder="Name" />
-                        <input type="email" id="email" name="email" placeholder="E-mail" />
-                        
-                    </div>
-                    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-                
-                    <input type="submit" value="Submit" />
+        <>
+            <div className="contact-text">
+                <h1>Contact Us</h1>
+                <p>Please feel free to contact us with any queries or issues, or you can leave a message bellow and someone from our team will reply.</p>
+            </div>
+            <div className="contact-main">
+                <div className="contact-info">
+                    <i class=""></i>
+                    <p>Address: 10 Mc Clure St, Belfast BT7 2FB</p>
+                    <p>Phone: 0044 (0)28 9901 6210</p>
+                    <p>Email: info@speedfixsales.com</p>
                 </div>
-            
-        </div>
+
+                <div classname="contact-form"> 
+                <Form className="Cform">
+                    <Form.Row>
+                        <Col>
+                            <Form.Control type="name" placeholder="Name" />
+                        </Col>
+                        <Col>
+                            <Form.Control typer="Email" placeholder="Email Address" />  
+                        </Col>
+                    </Form.Row>    
+                    <Form.Row>
+                        <Form.Control as="textarea" rows={3} placeholder="Message" />
+                         
+                    </Form.Row>
+                    <Button as="input" type="submit" value="Submit" />  
+                </Form>                                                                                                                                                           
+                </div>
+            </div>
+        </>
+             
+        
     )
 }
 
