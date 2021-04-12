92606c911d90f925ef1e3ee1c1368733d99e7852
amcmahon98 <75793615+amcmahon98@users.noreply.github.com>
2021-04-12 22:16:13 +0100

    Contact Page updates

--- src/component/page/Contact.js
+++ src/component/page/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Container, Form, Button, Card } from 'react-bootstrap';
 import './page-css/Contact.css';
+import SiteFooter from '../SiteFooter';
 
 function Contact() {
     return (
@@ -14,7 +15,7 @@ function Contact() {
                         <p>Phone: 0044 (0)28 9901 6210</p>
                         <p>Email: info@speedfixsales.com</p>
                     </Card.Text>
-                    <Form className="Cform" style={{padding: '3px'}}>
+                    <Form id="form"  className="Cform" style={{padding: '3px'}}>
                         <Form.Row className="row">
                             <Col>
                                 <Form.Control id="nameInput" type="name" placeholder="Name" />
@@ -28,14 +29,21 @@ function Contact() {
                         </Form.Row>
                             <Button
                                     onClick={event => {
-                                        document.getElementById("nameInput").value = ''
-                                        document.getElementById("emailInput").value = ''
-                                        document.getElementById("textInput").value = ''
-                                        alert("Thank you for contacting us! \n We aim to get back to you within 3 working days.");
+                                        document.getElementById("form").style.display="none"
+                                        alert("Thanks for contacting us!\nWe will aim to get back to you within 3 working days.");
                                     }}>Submit</Button>  
                     </Form> 
              </Card.Body>
-            </Card>    
+            </Card>   
+            <Card id="popup" className="popup"> 
+                <Card.Text style={{display : 'none' }}>
+                    Please feel free to contact us with any queries or issues, or you can leave a message bellow and someone from our team will reply.<p></p>
+                        <p>Address: 10 Mc Clure St, Belfast BT7 2FB</p>
+                        <p>Phone: 0044 (0)28 9901 6210</p>
+                        <p>Email: info@speedfixsales.com</p>
+                    </Card.Text>
+            </Card> 
+            <SiteFooter></SiteFooter> 
         </>
                    
     )
